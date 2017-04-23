ce2a065b365490a5bb4c4326663b6d0a70b16668
John Palomo Jr <palomo.john@hotmail.com>
2017-04-22 20:47:18 -0400

    Remove spaces in file name.

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-7za a -tzip "JP HUD mod friendly.pk3" *.txt* *.zscript* language.enu language.eng acs\ graphics\ scripts\ zscript\
+7za a -tzip "JP_HUD_mod_friendly.pk3" *.txt* *.zscript* language.enu language.eng acs\ graphics\ scripts\ zscript\
